cd073348b5411861f47704a83ce093a7f0f04bfc
mikmikinmoko <mikmikinmoko05@gmail.com>
2025-04-09 23:40:37 +0800

    update the constant

--- constant.lua
+++ constant.lua
@@ -1,6 +1,6 @@
 local set = {}
-local home = os.getenv("USERPROFILE")
+local home = os.getenv("USERPROFILE"):gsub("\\", "/")
 print(home)
-set.bg = home .. "\\.config\\wezterm\\assets\\dark.png"
+set.bg = home .. "/.config/wezterm/assets/the-nun.png"
 print(set.bg)
 return set
